8c75fdb72682cc716d50f0318c1d1c6d87023b20
arisia <starlights2283@gmail.com>
2023-09-30 11:32:32 +0900

    Add errorcode detection

--- 03_control_flow_statements/005-_switch_case/switch_case.java
+++ 03_control_flow_statements/005-_switch_case/switch_case.java
@@ -2,14 +2,37 @@ import java.util.Scanner;
 
 public class switch_case {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String error_type;
+        try (Scanner sc = new Scanner(System.in)) {
+            String error_type;
 
-        System.out.print("발생한 에러를 입력하세요(E0001, E0002, E0003, E0004, E0005) >> ");
-        error_type = sc.next();
-
-        
-
+            System.out.print("발생한 에러를 입력하세요(E0001, E0002, E0003, E0004, E0005) >> ");
+            error_type = sc.next();
+            switch(error_type){
+                case "e0001":
+                case "E0001":
+                    System.out.println("E0001E");
+                    break;
+                case "e0002":
+                case "E0002":
+                    System.out.println("E0002E");
+                    break;
+                case "e0003":
+                case "E0003":
+                    System.out.println("E0003E");
+                    break;
+                case "e0004":
+                case "E0004":
+                    System.out.println("E0004E");
+                    break;
+                case "e0005":
+                case "E0005":
+                    System.out.println("E0005E");
+                    break;
+                default:
+                    System.out.println("ERROR");
+                    break;
+            }
+        }
         int random_number = (int)(Math.random() * 100);
 
        
