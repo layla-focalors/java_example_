c81eebadf03012abdee425d245d9e45797ab39b7
arisia <starlights2283@gmail.com>
2023-09-30 11:26:11 +0900

    update

--- 03_control_flow_statements/003-_need_else_if/need_else_if.java
+++ 03_control_flow_statements/003-_need_else_if/need_else_if.java
@@ -1,33 +1,77 @@
+
 import java.util.Scanner;
 
 public class need_else_if {
     public static void main(String[] args) {
-        // import java.util.Scanner;
-        Scanner sc = new Scanner(System.in);
-        int num1 = 0, num2 = 0;
+        try (Scanner sc = new Scanner(System.in)) {
+            int num1 = 0, num2 = 0;
 
-        System.out.print("연산할 첫번째 수 > ");
-        
-        System.out.print("연산할 두번째 수 > ");
-        
+            System.out.print("연산할 첫번째 수 > ");
+            num1 = sc.nextInt();
+            System.out.print("연산할 두번째 수 > ");
+            num2 = sc.nextInt();
 
-        int operator = 0; 
-        System.out.print("연산을 선택하세요(1:덧셈/2:뺄셈/3:곱셈/4:나눗셈/5:나머지) : ");
-        
+            ///////////////////////////////////////// type1
+            // 덧셈 연산
+            System.out.println(num1+num2);
+            // 뺄셈 연산
+            System.out.println(num1-num2);
+            // 곱셈 연산
+            System.out.println(num1*num2);
+            // 나눗셈 연산
+            System.out.println((double)num1/num2);
+            ///////////////////////////////////////// type1
+            System.out.println(" ----------------------------------------------------------------- ");
 
-        int elseif_cnt = 0;
-        int if_cnt = 0;
+            int if_cnt = 0;     
+            int elseif_cnt = 0;
+            ///////////////////////////////////////// type2
+            int operator = 0; 
+            System.out.print("연산을 선택하세요(1:덧셈/2:뺄셈/3:곱셈/4:나눗셈/5:나머지) : ");
+            operator = sc.nextInt();
 
-        System.out.println("----------------------------------------------");
-        
+            // 연산되는 데이터의 숫자 보기
+            if(++if_cnt < 100 && operator == 1) { // 덧셈 연산
+                System.out.println(num1+num2);
+            }
+            if(++if_cnt < 100 && operator == 2) { // 뺄셈 연산
+                System.out.println(num1-num2);
+            }
+            if(++if_cnt < 100 && operator == 3) { // 곱셈 연산
+                System.out.println(num1*num2);
+            }
+            if(++if_cnt < 100 && operator == 4) { // 나눗셈 연산
+                System.out.println((double)num1/num2);
+            }
+            if(++if_cnt < 100 && operator == 5) { // 나머지 연산
+                System.out.println(num1%num2);
+            }
+            ///////////////////////////////////////// type2
+            System.out.printf("%s", if_cnt);
+            System.out.println(" ----------------------------------------------------------------- ");
+            ///////////////////////////////////////// type3
+            char char_operator;
+            System.out.print("연산을 선택하세요(+:덧셈 | -:뺄셈 | *:곱셈 | /:나눗셈 | %:나머지) : ");
+            char_operator = sc.next().charAt(0);
 
-        System.out.println("----------------------------------------------");
-        
-        
-        System.out.println("----------------------------------------------");
-        System.out.println("if_cnt의 값 : " + if_cnt);
-        System.out.println("elseif_cnt의 값 : " + elseif_cnt);
-
+            if(++elseif_cnt < 100 && char_operator == '+') { // 덧셈 연산
+                System.out.println(num1+num2);
+            }
+            else if(++elseif_cnt < 100 && char_operator == '-') { // 뺄셈 연산
+                System.out.println(num1-num2);
+            }
+            else if(++elseif_cnt < 100 && char_operator == '*') { // 곱셈 연산
+                System.out.println(num1*num2);
+            }
+            else if(++elseif_cnt < 100 && char_operator == '/') { // 나눗셈 연산
+                System.out.println((double)num1/num2);
+            }
+            else if(++elseif_cnt < 100 && char_operator == '%') { // 나머지 연산
+                System.out.println(num1%num2);
+            }
+            System.out.printf("%s", elseif_cnt);
+        }
+        ///////////////////////////////////////// type3
         return;
     }
 }
