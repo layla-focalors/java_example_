08cedbdf969214db6711cec591258917bcb08fa7
arisia <starlights2283@gmail.com>
2023-09-30 12:04:51 +0900

    add updates

--- 03_control_flow_statements/010-_break/Break.java
+++ 03_control_flow_statements/010-_break/Break.java
@@ -1,28 +1,14 @@
-import java.util.Scanner;
-
 public class Break {
     public static void main(String[] args) {
         
-        // for(int i=0; i<2000; i++) {
-        //     if(i==100) { 
-        //         System.out.println("i의 값 " + i + "에서 탈출합니다.");
-        //         break;
-        //     }
-        //     System.out.println("i의 값은 " + i);
-        // }
-
-        
-
-
-
-
-
-
-
-
-
-
-        
+        for(int i=0; i<2000; i++) {
+            if(i==100) { 
+                System.out.println("i의 값 " + i + "에서 탈출합니다.");
+                break;
+            }
+            System.out.println("i의 값은 " + i);
+        }        
+        // 반복문이 100일 때, 탈출!
         return;
     }
 }
