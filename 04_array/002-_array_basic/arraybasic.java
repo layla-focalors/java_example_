15327f3a5f063fb45f9d66eb50fbb625dd4ae3d7
arisia <starlights2283@gmail.com>
2023-09-30 12:29:58 +0900

    add array example

--- 04_array/002-_array_basic/arraybasic.java
+++ 04_array/002-_array_basic/arraybasic.java
@@ -7,30 +7,41 @@ public class arraybasic {
         // 국어
         int[] score1 = new int[5];
         // 영어
+        int score2[] = new int[5];
         // 수학
-        
+        int[] score3;
+        score3 = new int[5];
         // 외국어
-        
-        Scanner sc = new Scanner(System.in);
+        int[] score4 = new int[] {100,100,100,100,100};    
+        try (Scanner sc = new Scanner(System.in)) {
+            // 반복문을 통한 데이터 입력
+            // 국어 점수 입력
+            for(int i=0; i<5; i++) {
+                System.out.print(i+1+"번째 학생의 국어 점수를 입력 >> ");
+                score1[i] = sc.nextInt();
+            }
+            // 수학 점수 입력
+            for(int i=0; i<5; i++) {
+                System.out.print(i+1+"번째 학생의 수학 점수를 입력 >> ");
+                score2[i] = sc.nextInt();
+            }
+            // 영어
+            for(int i=0; i<5; i++) {
+                System.out.print(i+1+"번째 학생의 영어 점수를 입력 >> ");
+                score3[i] = sc.nextInt();
+            }
 
-        // 반복문을 통한 데이터 입력
-        // 국어 점수 입력
-        for(int i=0; i<5; i++) {
-            System.out.print(i+1+"번째 학생의 국어 점수를 입력 >> ");
-            score1[i] = sc.nextInt();
         }
-        // 수학 점수 입력
-        
-        // 영어 점수 입력
-        
 
         System.out.println("--------------------------------------------------------------------");
         // 국어 점수 출력
-        for(int i=0; i<5; i++) {
+        for(int i=0; i<score1.length; i++) {
             System.out.println(i+1+"번째 학생의 국어 점수를 출력 >> " + score1[i]);
         }
         // 수학 점수 출력
-        
+        for(int i=0; i<score2.length; i++) {
+            System.out.println(i+1+"번째 학생의 수학 점수를 출력 >> " + score2[i]);
+        }
         // 영어 점수 출력
         for(int i=0; i<score3.length; i++) {
             System.out.println(i+1+"번째 학생의 영어 점수를 출력 >> " + score3[i]);
