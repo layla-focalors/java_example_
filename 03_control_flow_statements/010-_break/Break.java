dfb866dda6e0c75ee7a1704927768be136eefa73
arisia <starlights2283@gmail.com>
2023-09-30 12:08:26 +0900

    add flow

--- 03_control_flow_statements/010-_break/Break.java
+++ 03_control_flow_statements/010-_break/Break.java
@@ -9,6 +9,16 @@ public class Break {
             System.out.println("i의 값은 " + i);
         }        
         // 반복문이 100일 때, 탈출!
+        for(int i = 0; i < 2; i ++){
+            for(int j=0; j < 1000; j ++){
+                if(j==100){
+                    System.out.println("탈출!");
+                    break;
+                }
+                System.out.println("A");
+            }
+            System.out.println("B");
+        }
         return;
     }
 }
--- 03_control_flow_statements/011-_continue/Continue.java
+++ 03_control_flow_statements/011-_continue/Continue.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
-
 public class Continue {
     public static void main(String[] args) {
         
         for(int i=0; i<100; i++) {
-         
+            if(i % 2 == 0){
+                continue;
+            }
+        }
         return;
     }
 }
