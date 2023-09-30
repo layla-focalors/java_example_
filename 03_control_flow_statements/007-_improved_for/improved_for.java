a54b9a74ff964b19acbe0b2c9b6b4507c6afe18b
arisia <starlights2283@gmail.com>
2023-09-30 11:55:28 +0900

    add round2x

--- 03_control_flow_statements/007-_improved_for/improved_for.java
+++ 03_control_flow_statements/007-_improved_for/improved_for.java
@@ -4,6 +4,11 @@ public class improved_for {
     public static void main(String[] args) {
         
         int[] arr = {1,2,3,4,5,6,7,8,9};
+        // new for 문
+        for(int e : arr){
+            System.out.println(e);
+            // 구성원의 값이 e로 들어가짐!
+        }
 
         
         return;
